cc7235875aa4d559f2aa0b2c4a9c52244053e20b
Chineek Favors <chineekfavors01@gmail.com>
2020-02-02 22:32:36 -0800

    implemented put requestAPI endpoint

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -15,12 +15,33 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req,res) => {
     const {id} = req.params;
     try {
-        car = await db('cars',).where('id', id);
+        const car = await db('cars',).where('id', id);
         res.json(car);
     } catch(err){
         res.status(500).json({errorMessage: 'there was a problem retrieving data from database'})
     }
-})
+});
+
+router.post('/', async (req,res) => {
+    const body = req.body;
+    try {
+        const postCar = await db('cars').insert(body);
+        res.status(201).json(postCar);
+    } catch(err) {
+        res.status(500).json({errorMessage: 'there was a problem posting to database'});
+    }
+});
+
+router.put('/:id', async (req,res) => {
+    const {id} = req.params;
+    const updateCar = req.body;
+    try {
+        const car = await db('cars').where('id', id).update(updateCar);
+        res.status(201).json({updated: car});
+    } catch(err) {
+        res.status(500).json({errorMessage: 'there was a problem updating database'});    
+    }    
+});
 
 
 module.exports = router;
