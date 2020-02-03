eda62a54aba44272a4b76588314e4e81dad9c384
Chineek Favors <chineekfavors01@gmail.com>
2020-02-02 22:09:50 -0800

    created end point for get request

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const db = require('../data/db-config.js');
+
+const router = express();
+
+router.get('/', async (req, res) => {
+    try {
+        const cars = await db('cars');
+        res.json(cars);
+    } catch(err){
+        res.status(500).json({errorMessage: 'there was a problem retrieving cars data from database'});
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
-
+const carsRouter = require('./cars/carsRouter.js');
 
 const server = express();
 
 server.use(express.json());
 
 
+ server.use('/cars', carsRouter);
 
 server.get('/', (req,res) => {
     res.status(200).send('<h1>Node-db2-project</h2>');
